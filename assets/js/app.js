9f94e8993c6dc10997215de8d91f703f939c419a
parth sharma <parthsharma@gmail.com>
2024-03-08 14:09:12 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+let accordian = document.querySelectorAll(".full-part")
+let img = document.querySelector(".accord-img");
+let txt = document.querySelector(".accord-para");
+img.style.transform = "rotate(180deg)";
+txt.style.display = "block";
+accordian.forEach(element => {
+    let img = element.querySelector(".accord-img");
+    let clicable = element.querySelector(".top-part");
+    let txt = element.querySelector(".accord-para");
+    clicable.addEventListener("click", () => {
+        let disp_text = window.getComputedStyle(txt).display;
+        accordian.forEach(otherelement => {
+            if (otherelement !== element) {
+                let txt = otherelement.querySelector(".accord-para");
+                let img = otherelement.querySelector(".accord-img");
+                txt.style.display = "none";
+                img.style.transform = "rotate(0deg)";
+            }
+            if (disp_text === "none") {
+                txt.style.display = "block";
+                img.style.transform = "rotate(180deg)";
+            } else {
+                txt.style.display = "none";
+                img.style.transform = "rotate(0deg)";
+            }
+        });
+    });
+});
