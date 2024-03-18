0cc25820dc51dc8c266e2ecb62fc553ac745ba71
parth sharma <parthsharma@gmail.com>
2024-03-18 10:25:05 +0530

    NavBar Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,13 @@ accordian.forEach(element => {
         });
     });
 });
+
+// NavBar
+
+const menuIcon = document.querySelector(".menuIcon");
+const menuList = document.querySelector(".smView");
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("show");
+    document.querySelector("nav").classList.toggle("navbg");
+    document.querySelector("body").classList.toggle("overflow-hidden");
+})
